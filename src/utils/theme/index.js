2b527b5268ca9f6df0bc1affa196ef3af16ea5fb
zlc <460544102@qq.com>
2026-06-25 17:53:44 +0800

    按钮颜色修改

--- src/utils/theme/index.js
+++ src/utils/theme/index.js
@@ -5,6 +5,64 @@ import { scopedLocalStorage as localStorage } from '@/utils/storage'
 
 let originalStyle = ''
 
+const SEMANTIC_BUTTONS = {
+  primary: {
+    colorVar: '--color-primary',
+    disabledBorder: '#BAD6FF',
+    disabledText: '#BAD6FF',
+    disabledBg: '#F0F7FF'
+  },
+  warning: {
+    colorVar: '--color-warning',
+    disabledBorder: '#FFDAAD',
+    disabledText: '#FFDAAD',
+    disabledBg: '#FFF9F0'
+  },
+  success: {
+    colorVar: '--color-success',
+    disabledBorder: '#8FEBAB',
+    disabledText: '#8FEBAB',
+    disabledBg: '#EDFFF1'
+  },
+  danger: {
+    colorVar: '--color-danger',
+    disabledBorder: '#FFC6BF',
+    disabledText: '#FFC6BF',
+    disabledBg: '#FFF2F0'
+  }
+}
+
+function buildSemanticButtonStyles() {
+  let css = ''
+  for (const [type, config] of Object.entries(SEMANTIC_BUTTONS)) {
+    const color = `var(${config.colorVar})`
+    // 全局 mixin 默认给 el-button 加 is-plain，样式只针对 .is-plain 即可
+    css += `
+      .el-button--${type}.is-plain:not(.is-disabled) {
+        color: ${color}!important;
+        border-color: ${color}!important;
+        background-color: #fff!important;
+      }
+      .el-button--${type}.is-plain:not(.is-disabled):hover,
+      .el-button--${type}.is-plain:not(.is-disabled):focus,
+      .el-button--${type}.is-plain:not(.is-disabled):active {
+        color: #fff!important;
+        background-color: ${color}!important;
+        border-color: ${color}!important;
+      }
+      .el-button--${type}.is-plain.is-disabled,
+      .el-button--${type}.is-plain.is-disabled:hover,
+      .el-button--${type}.is-plain.is-disabled:focus,
+      .el-button--${type}.is-plain.is-disabled:active {
+        color: ${config.disabledText}!important;
+        border-color: ${config.disabledBorder}!important;
+        background-color: ${config.disabledBg}!important;
+      }
+    `
+  }
+  return css
+}
+
 export function changeElementColor() {
   let colorsCssText = ''
   let cssText = originalStyle
@@ -17,27 +75,9 @@ export function changeElementColor() {
     .border-${key}{border-color: ${value}!important;}
     `
     if (['primary', 'success', 'info', 'warning', 'danger'].includes(key)) {
-      const blendColor = mix('ffffff', value.replace(/#/g, ''), 35)
       const darken = mix('000000', value.replace(/#/g, ''), 10)
       const tooLightColor = mix('ffffff', value.replace(/#/g, ''), 90)
-      const buttonHoverBg = key === 'primary' ? value : darken
-      const buttonHoverBorder = key === 'primary' ? value : 'var(--color-border)'
       colorsCssText = colorsCssText + `
-        .el-button--${key}:not(.is-plain){
-          border-color: var(--color-border);
-        }
-        .el-button--${key}:not(.is-plain):focus,
-        .el-button--${key}:not(.is-plain):active,
-        .el-button--${key}:not(.is-plain):hover {
-          background-color: ${buttonHoverBg};
-          border-color: ${buttonHoverBorder};
-        }
-        .el-button--${key}.is-disabled:not(.is-plain),
-        .el-button--${key}.is-disabled:not(.is-plain):active,
-        .el-button--${key}.is-disabled:not(.is-plain):focus {
-          background-color: ${blendColor}!important;
-          border-color: var(--color-border);
-        }
         .el-link.el-link--${key}{
           color: ${value}!important;
         }
@@ -57,6 +97,8 @@ export function changeElementColor() {
       `
     }
   }
+
+  colorsCssText += buildSemanticButtonStyles()
 
   colorsCssText = colorsCssText.replaceAll('\n', '')
   let styleTag = document.getElementById('themeStyle')
